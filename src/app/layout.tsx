7a8868ddf309cf3e1e71e4e83c0a02456f5dbd47
Mohammed Moudachir BOURAIMA <mbouraima@fidra.ci>
2025-06-27 17:58:18 +0000

    Nouvelle mis à jour effectué sur la sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { roboto } from "@/font/font";
 import { Providers } from "./providers";
-import { ThemeModeScript } from 'flowbite-react';
 
 
 export const metadata: Metadata = {
@@ -16,9 +15,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <head>
-        <ThemeModeScript />
-      </head>
       <body className={`${roboto.className}`}>
         <Providers>{children}</Providers>
       </body>
